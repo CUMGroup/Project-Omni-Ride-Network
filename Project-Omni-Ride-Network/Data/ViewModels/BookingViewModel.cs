787153230e52a06cfd4d2ecad55ac733abf676fc
Alex Hagl <alex@hagl.de>
2022-06-04 12:25:43 +0200

    PORN-44: Added ViewModels for booking

---
+++ Project-Omni-Ride-Network/Data/ViewModels/BookingViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Project_Omni_Ride_Network {
+    public class BookingViewModel {
+
+        public Vehicle Vehicle { get; set; }
+
+        public BookingViewModel(Vehicle v) {
+            Vehicle = v;
+        }
+
+    }
+}
