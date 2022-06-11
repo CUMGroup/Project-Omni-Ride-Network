23ea20a5bd920da89471314c4b627add44666692
jas20202 <99250573+jas20202@users.noreply.github.com>
2022-06-11 04:11:38 +0200

    PORN-22: preisberechnung 50%

--- Project-Omni-Ride-Network/wwwroot/js/preisberechnung.js
+++ Project-Omni-Ride-Network/wwwroot/js/preisberechnung.js
@@ -1,10 +1,22 @@
-﻿let gesamtpreis = 0;
-
-
+﻿
 function inputchange(basicprice, priceHD, priceIns) {
     console.log("change happened");
     
+    let gesamtpreis;
+    gesamtpreis = basicprice;
 
+    if ($('#vollkasko')[0].checked) {
+        console.log(priceIns);
+        gesamtpreis += Number(priceIns);
+    } else if ($('#teilkasko')[0].checked){
+        gesamtpreis += 50;
+    }
+
+    if ($('#refuel')[0].checked) {
+        gesamtpreis += 35;
+    }
+
+    $('#gesamtpreis')[0].innerText = gesamtpreis;
 }
 
 function checkinsurance(uncheck) {
