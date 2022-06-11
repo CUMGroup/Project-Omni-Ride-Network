c7cd230fef9083cd7e3b845e48555958b1518598
jas20202 <99250573+jas20202@users.noreply.github.com>
2022-06-11 16:25:18 +0200

    PORN-22: finished preisberechnung

--- Project-Omni-Ride-Network/wwwroot/js/preisberechnung.js
+++ Project-Omni-Ride-Network/wwwroot/js/preisberechnung.js
@@ -1,13 +1,17 @@
 ﻿
+
+
 function inputchange(basicprice, priceHD, priceIns) {
     console.log("change happened");
-    
+
+    console.log($("#pickupdate")[0].value);
+
     let gesamtpreis;
-    gesamtpreis = basicprice;
+    gesamtpreis = fixNumber(basicprice);
 
     if ($('#vollkasko')[0].checked) {
-        console.log(priceIns);
-        gesamtpreis += Number(priceIns);
+        console.log(fixNumber(priceIns));
+        gesamtpreis += fixNumber(priceIns);
     } else if ($('#teilkasko')[0].checked){
         gesamtpreis += 50;
     }
@@ -16,9 +20,44 @@ function inputchange(basicprice, priceHD, priceIns) {
         gesamtpreis += 35;
     }
 
-    $('#gesamtpreis')[0].innerText = gesamtpreis;
+    let dates = calculateDate($("#pickupdate")[0].value, $("#returndate")[0].value);
+
+    gesamtpreis += dates * fixNumber(priceHD);
+
+    $('#gesamtpreis')[0].innerText = gesamtpreis.toFixed(2);
 }
 
 function checkinsurance(uncheck) {
     uncheck[0].checked = false;
+}
+
+function fixNumber(number) {
+
+    numberFixed = number.replace(",", ".")
+
+    return Number(Number(numberFixed).toFixed(2));
+}
+
+function calculateDate(pickupdate, returndate) {
+    if (pickupdate == "" || returndate == "") {
+        console.log("no date");
+        return 1;
+    } else {
+        /*pickMonth = pickupdate.substring(5, 7);
+        returnMonth = returndate.substring(5, 7);
+
+        pickDay = pickupdate.substring(8);
+        returnDay = returndate.substring(8);
+
+        if (pickMonth == returnMonth) {
+            return (returnDay - pickDay) + 1;
+        }*/
+
+        pickupdate = new Date(pickupdate);
+        returndate = new Date(returndate);
+
+        return Math.round((returndate - pickupdate) / 1000 / 60 / 60 / 24);
+
+        console.log(Math.round((returndate - pickupdate) / 1000 / 60 / 60 / 24));
+    }
 }
