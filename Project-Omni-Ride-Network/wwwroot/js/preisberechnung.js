f7e6d1a0db279531a265b665ef086a8a5059b7ab
jas20202 <99250573+jas20202@users.noreply.github.com>
2022-06-11 03:45:28 +0200

    PORN-22: progress

--- Project-Omni-Ride-Network/wwwroot/js/preisberechnung.js
+++ Project-Omni-Ride-Network/wwwroot/js/preisberechnung.js
@@ -1,3 +1,12 @@
-﻿inputchange(){
+﻿let gesamtpreis = 0;
 
+
+function inputchange(model) {
+    console.log("change happened");
+    console.log(model);
+
+}
+
+function checkinsurance(uncheck) {
+    uncheck[0].checked = false;
 }
