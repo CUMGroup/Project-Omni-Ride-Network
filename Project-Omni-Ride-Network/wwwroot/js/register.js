d97e99aa02be385e63e84115995b1be945f0e934
jas20202 <99250573+jas20202@users.noreply.github.com>
2022-06-14 00:02:54 +0200

    PORN-33: implemented check if password repetition is correct

--- Project-Omni-Ride-Network/wwwroot/js/register.js
+++ Project-Omni-Ride-Network/wwwroot/js/register.js
@@ -1,1 +1,18 @@
-﻿
+﻿function checkPW_RPT(pw, pwRPT) {
+    console.log(pw);
+
+    if (!(pw == pwRPT)) {
+        $('#PasswordRPT')[0].classList = "loginpage user RPTincorrect";
+        $('#RPTincorrect')[0].innerText = "Passwörter stimmen nicht überein."
+    }
+    else {
+        $('#PasswordRPT')[0].classList = "loginpage user";
+        $('#RPTincorrect')[0].innerText = ""
+    }
+}
+
+function checkPW_norm(pw) {
+    if (pw.includes()) {
+
+    }
+}
