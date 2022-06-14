c11290c379aaa607cd48220c5285488ab0e4a012
jas20202 <99250573+jas20202@users.noreply.github.com>
2022-06-14 11:13:18 +0200

    PORN-33: added dropdown menus

--- Project-Omni-Ride-Network/wwwroot/js/register.js
+++ Project-Omni-Ride-Network/wwwroot/js/register.js
@@ -1,13 +1,13 @@
 ﻿function checkPW_RPT(pw, pwRPT) {
-    console.log(pw);
-
     if (!(pw == pwRPT)) {
         $('#PasswordRPT')[0].classList = "loginpage user RPTincorrect";
         $('#RPTincorrect')[0].innerText = "Passwörter stimmen nicht überein."
+        return false;
     }
     else {
         $('#PasswordRPT')[0].classList = "loginpage user";
         $('#RPTincorrect')[0].innerText = ""
+        return true;
     }
 }
 
@@ -18,10 +18,12 @@ function checkPW_norm(pw) {
     if (password.length < 6 || !(password.match(criteria))) {
         $('#Password')[0].classList = "loginpage user RPTincorrect";
         $('#pwCriteria')[0].classList = "registerpage RPTincorrect";
+        return false;
     }
     else {
         $('#Password')[0].classList = "loginpage user";
         $('#pwCriteria')[0].classList = "registerpage passwordRQMTS";
+        return true;
     }
 }
 
@@ -34,9 +36,22 @@ function check_age() {
     if (age < 18) {
         $('#KdBirth')[0].classList = "loginpage user RPTincorrect";
         $('#underage')[0].innerText = "Du musst mindestens 18 sein um bei uns ein Account anzulegen."
+        return false;
     }
     else {
         $('#KdBirth')[0].classList = "loginpage user";
         $('#underage')[0].innerText = ""
+        return true;
     }
+}
+
+function check_all() {
+    if (
+        checkPW_norm($('#Password')[0].value) &
+        checkPW_RPT($('#Password')[0].value, $('#PasswordRPT')[0].value) &
+        check_age()
+    ) {
+        return true;
+    }
+    return false;
 }
